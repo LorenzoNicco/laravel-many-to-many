884ccd3ebef91310ca8a6d1a79f6b7a4d1820c27
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-03-28 21:18:06 +0200

    Fix show technology

--- resources/views/admin/technologies/show.blade.php
+++ resources/views/admin/technologies/show.blade.php
@@ -3,19 +3,19 @@
 @section('content')
     <div class="container-fluid mt-4">
         <div class="row justify-content-center">
-            <h1 class="text-center text-light my-5">Dettagli tipologia</h1>
+            <h1 class="text-center text-light my-5">Dettagli tecnologia</h1>
 
             <div class="card">
                 <div class="col-md-8">
-                    <h1 class="mb-3">{{ $type->title }}</h1>
+                    <h1 class="mb-3">{{ $technology->name }}</h1>
         
-                    <h6 class="mb-3">{{ $type->slug }}</h6>
+                    <h6 class="mb-3">{{ $technology->slug }}</h6>
 
-                    <h3 class="mb-3">Progetti associati: {{ $type->projects()->count() }}</h3>
+                    <h3 class="mb-3">Progetti associati: {{ $technology->projects()->count() }}</h3>
 
-                    @if ($type->projects()->count() > 0)
+                    @if ($technology->projects()->count() > 0)
                         <ul>
-                            @foreach ($type->projects as $project)
+                            @foreach ($technology->projects as $project)
                                 <li>
                                     <a href="{{ route('admin.projects.show', $project->id) }}">{{ $project->title }}</a>
                                 </li>
